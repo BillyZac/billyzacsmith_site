f41a34c2435dc288036b7cd4963bb4040384b33b
Zachary Smith <billyzacsmith@gmail.com>
2016-01-24 11:55:51 -0700

    Tiled images kind of sort of make a face that you can look at. Neat!

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 var app = angular.module('app', [])
 
 app.controller('MainController', function($scope) {
-  var tile = {
+  var tile1 = {
     image_url: 'http://www.kylelambert.co.uk/gallery/adobe-ideas-eye/images/kyle-lambert-adobe-ideas-eye-ipad-painting-small.jpg'
   }
-  $scope.rows = [[1, 2, 3], [4, tile, 6], [7, 8, 9]]
+  var tile2 = {
+    image_url: 'http://www.daz3d.com/media/catalog/product/cache/1/thumbnail/689303033aebc8cae535000c73c8db4b/l/u/lunar-craters-moonscape-2.jpg'
+  }
+  var tile3 = {
+    image_url: 'https://c2.staticflickr.com/8/7011/6398822869_4cde35711e_b.jpg'
+  }
+  var tile4 = {
+    image_url: 'http://rs1228.pbsrc.com/albums/ee443/storylockup1/RLC/textures/6964131-close-up-of-wolf-fur_zpsf97dcf6f.jpg~c200'
+  }
+  var tile5 = {
+    image_url: 'http://fullyfeline.com/wp-content/uploads/2015/07/Cat-nose-close-up-small-200x200.jpg'
+  }
+  var tile6 = {
+    image_url: 'http://rs19.pbsrc.com/albums/b172/nekoaitsu/Earthbound%20Pics/CIMG1678.jpg~c200'
+  }
+  var tile7 = {
+    image_url: 'http://rs11.pbsrc.com/albums/a183/marleenbryan/moon%20geology/800px-Tycho_Crater.jpg~c200'
+  }
+  var tile8 = {
+    image_url: 'http://new.56060.in/CMS/Previews/ANIM/big/ANI815525.gif'
+  }
+  var tile9 = {
+    image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/PIA17211-SaturnMoon-Enceladus-CloseFlyby-20151219.jpg/200px-PIA17211-SaturnMoon-Enceladus-CloseFlyby-20151219.jpg'
+  }
+  $scope.rows = [
+    [tile1, tile2, tile3],
+    [tile4, tile5, tile6],
+    [tile7, tile8, tile9]]
 
 })
