7f907a373c4426be02e99e4c5f63e87431c279e1
Zachary Smith <billyzacsmith@gmail.com>
2016-02-29 08:26:39 -0700

    Add home folder with home controller. Forgot to add this on last commit. Oops.

---
+++ public/app/home/home.controller.js
@@ -0,0 +1,23 @@
+angular.module('app')
+.controller('HomeController', HomeController, ['$scope'])
+
+function HomeController($scope) {
+  var vm = this
+
+  var isms = [
+    'is a designer and front end developer in Denver, Colorado.',
+    'likes Cinnamon Toast Crunch with half & half.',
+    'grew up in the sticks.'
+  ]
+  
+  vm.tagline = isms[0] // So don't have to wait for first timeout.
+  var index = 0
+  setInterval(function () {
+    if (index >= isms.length) {
+      index = 0
+    }
+    vm.tagline = isms[index]
+    $scope.$digest()
+    index++
+  }, 3000)
+}
