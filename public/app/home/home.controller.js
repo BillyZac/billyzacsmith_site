b931333453b73bcd2e5e02abf026cbff6960f488
Zachary Smith <billyzacsmith@gmail.com>
2016-03-02 07:37:40 -0700

    Major restructure of nav and header.
    Header is an fixed position element that covers the entire screen with an opacity of 95%. Tagline displays based on conditional logic: only visible on home page. (There is some latency issue with this. The tagline sticks around for a half second or so when you go to one of the non-home pages.)

--- public/app/home/home.controller.js
+++ public/app/home/home.controller.js
@@ -2,29 +2,29 @@ angular.module('app')
 .controller('HomeController', HomeController, ['$scope'])
 
 function HomeController($scope) {
-  var vm = this
-
-  var isms = [
-    'is a designer and web developer in Denver, Colorado.',
-    'likes kale, but loves Cinnamon Toast Crunch with half & half.',
-    'grew up in the sticks.',
-    'thinks Robert Bringhurst is the bee\'s knees.',
-    'prefers dawn to dusk, but only slightly.',
-    'illustrated a children\'s book.',
-  ]
-
-  vm.tagline = isms[0] // So don't have to wait for first timeout.
-  var index = 0
-  setInterval(function () {
-    if (index >= isms.length) {
-      index = 0
-    }
-    vm.tagline = isms[index]
-    $scope.$digest()
-    index++
-  }, 3000)
-
-  // Chart
-  vm.chartData = [5, 10, 15, 20, 25, 17, 3, 46]
-  // $scope.chartData = [5, 10, 15, 20, 25, 17, 3, 46]
+  // var vm = this
+  //
+  // var isms = [
+  //   'is a designer and web developer in Denver, Colorado.',
+  //   'likes kale, but loves Cinnamon Toast Crunch with half & half.',
+  //   'grew up in the sticks.',
+  //   'thinks Robert Bringhurst is the bee\'s knees.',
+  //   'prefers dawn to dusk, but only slightly.',
+  //   'illustrated a children\'s book.',
+  // ]
+  //
+  // vm.tagline = isms[0] // So don't have to wait for first timeout.
+  // var index = 0
+  // setInterval(function () {
+  //   if (index >= isms.length) {
+  //     index = 0
+  //   }
+  //   vm.tagline = isms[index]
+  //   $scope.$digest()
+  //   index++
+  // }, 3000)
+  //
+  // // Chart
+  // vm.chartData = [5, 10, 15, 20, 25, 17, 3, 46]
+  // // $scope.chartData = [5, 10, 15, 20, 25, 17, 3, 46]
 }
